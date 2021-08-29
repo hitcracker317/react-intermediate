849046957ad04f3c1d33a0d1851f7e884552a941
hitcracker317 <dainaso97@gmail.com>
2021-08-29 18:54:54 +0900

    ユーザー一覧実装

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,36 @@
-import React, { memo, VFC } from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import { memo, useEffect, VFC } from 'react'
+import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+
+import { UserCard } from "../organisms/user/UserCard";
+import { useAllUsers } from "../../hooks/useAllUsers";
 
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザー管理画面です</p>
+  const { getUsers, loading, users } = useAllUsers();
+
+  useEffect(() => getUsers(), []);
+
+  return (
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner color="teal.200" />
+        </Center>
+      ) : (
+        // wrap：https://chakra-ui.com/docs/layout/wrap
+        // いい感じのレスポンシブで横並びしてくれる
+        <Wrap p={{ base: 4, md: 10 }}>
+          {users.map(user => (
+            <WrapItem key={user.id} mx="auto">
+              <UserCard
+                imageUrl="https://source.unsplash.com/random"
+                userName={user.username}
+                fullName={user.name}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
+  );
 })
